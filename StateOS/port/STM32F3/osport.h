f0957f4717a46278a8617ec396afb6bd4e0bb545
Rajmund Szymanski <rajszym@o2.pl>
2016-03-09 11:03:57 +0100

    update osport

--- StateOS/port/STM32F3/osport.h
+++ StateOS/port/STM32F3/osport.h
@@ -2,7 +2,7 @@
 
     @file    StateOS: osport.h
     @author  Rajmund Szymanski
-    @date    08.03.2016
+    @date    09.03.2016
     @brief   StateOS port definitions for STM32F3 uC.
 
  ******************************************************************************
